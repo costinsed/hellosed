0280ff12f638a5970e9c369c3167b2de0bb0248e
costinsed <79978124+costinsed@users.noreply.github.com>
2021-03-04 14:51:23 +0100

    Add files via upload

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str_HelloWorld = "HelloWorld";
+            Console.WriteLine($"My sentence follows :{str_HelloWorld}");
+            Console.ReadKey();            
+        }
+    }
+}
